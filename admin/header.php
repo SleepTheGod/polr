2fbf2a7b1c365312c2cf2307a5ba86f682091213
Taylor Christian Newsome <86472964+SleepTheGod@users.noreply.github.com>
2024-03-11 00:49:12 -0400

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -14,61 +14,58 @@
 -->
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title><?php require_once('../config.php');echo $wsn;?></title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title><?php require_once('../config.php'); echo htmlspecialchars($wsn, ENT_QUOTES, 'UTF-8'); ?></title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php if (!$theme) {echo '../css/bootstrap.css';}else {echo $theme;}?>"/>
+    <link rel="stylesheet" href="<?php echo !$theme ? '../css/bootstrap.min.css' : htmlspecialchars($theme, ENT_QUOTES, 'UTF-8'); ?>"/>
     <link rel="stylesheet" href="../css/main.css"/>
-    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="shortcut icon" href="../favicon.ico">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
-
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.1/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css" />
 </head>
 <body style="padding-top:60px">
     <div class="container-fluid">
-        <nav role="navigation" class="navbar navbar-inverse navbar-fixed-top">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-
-            <div class="navbar-header">
-                <a class="navbar-brand" href="../index.php"><?php echo $wsn;?></a>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="../index.php"><?php echo htmlspecialchars($wsn, ENT_QUOTES, 'UTF-8'); ?></a>
+                <div class="collapse navbar-collapse" id="navbar">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="../about.php">About</a></li>
+                    </ul>
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <?php require_once('../lib-auth.php');
+                        $polrauth = new polrauth();
+                        $polrauth->headblock(); ?>
+                        <?php require_once('../config.php'); if ($regtype != 'none') { echo '<li class="nav-item"><a class="nav-link" href="../register.php">Sign Up</a></li>'; } ?>
+                        <!-- Login Dropdown -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Sign In</a>
+                            <div class="dropdown-menu dropdown-menu-end" style="padding: 15px;">
+                                <form action="../handle-login.php" method="post" accept-charset="UTF-8">
+                                    <div class="mb-3">
+                                        <label for="user_username" class="form-label">Username</label>
+                                        <input id="user_username" type="text" name="username" placeholder="Username" class="form-control">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="user_password" class="form-label">Password</label>
+                                        <input id="user_password" type="password" name="password" placeholder="Password" class="form-control">
+                                    </div>
+                                    <button type="submit" name="login" class="btn btn-success w-100">Sign In</button>
+                                </form>
+                            </div>
+                        </li>
+                        <?php $polrauth->headendblock(true); ?>
+                    </ul>
+                </div>
             </div>
-
-            <ul id="navbar" class="nav navbar-collapse collapse navbar-nav" id="nbc">
-                <li><a href="../about.php">About</a></li>
-                <li class="visible-xs"><a href="index.php">Sign In/Dashboard</a></li>
-            </ul>
-            <ul class="nav pull-right navbar-nav hidden-xs">
-                <?php require_once('../lib-auth.php');
-                $polrauth = new polrauth();
-                $polrauth->headblock(); ?>
-                <?php require_once('../config.php'); if ($regtype != 'none'){ echo '<li><a href="../register.php">Sign Up</a></li>';}?>
-                <li class="divider-vertical"></li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
-                    <div class="dropdown-menu pull-right" id="dropdown" style="padding: 15px; padding-bottom: 0px;">
-                        <h2>Login</h2>
-                        <form action="../handle-login.php" method="post" accept-charset="UTF-8">
-                            <input id="user_username" style="margin-bottom: 15px;" type="text" name="username" placeholder='Username' size="30" class="form-control">
-                            <input id="user_password" style="margin-bottom: 15px;" type="password" name="password" placeholder='Password' size="30" class="form-control">
-                            <input class="btn btn-success form-control" style="clear: left; width: 100%; height: 32px; font-size: 13px;" type="submit" name="login" value="Sign In">
-                            <br><br>
-                        </form>
-                    </div>
-                </li>
-<?php $polrauth->headendblock(true); ?>
-
-            </ul>
-        </div>
+        </nav>
     </div>
     <div class="container">
         <div class="jumbotron" style="text-align:center; padding-top:80px; background-color: rgba(0,0,0,0);">
